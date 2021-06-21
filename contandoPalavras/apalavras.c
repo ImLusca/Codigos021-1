7b8adbd4c962f26f5c623dbbc5c45eca55a0a1ed
Lucasbr2312 <lucaspacheco747@gmail.com>
2021-06-21 15:08:16 -0300

    .

--- contandoPalavras/apalavras.c
+++ contandoPalavras/apalavras.c
@@ -1,9 +1,19 @@
 #include<stdio.h>
 
-int VerificaSeeLetra(char caractere){
-    int maiuscula = (caractere >= 65 && caractere <= 90);
-    int minuscula = (caractere >= 97 && caractere<= 122);
-    return (maiuscula || minuscula);
+int VerificaSeEValido(char caractere){
+    // int maiuscula = (caractere >= 65 && caractere <= 90);
+    // int minuscula = (caractere >= 97 && caractere<= 122);
+    // return (maiuscula || minuscula);
+
+    switch (caractere)
+    {
+        case ' ':return 0;        
+        case '\r': return 0;
+        case '\t': return 0;
+        case '\n': return 0;    
+        default: return 1;        
+    }
+
 }
 
 
@@ -11,27 +21,35 @@ int main(){
     int ContPalavras=0, ContCaracteres=0,ContLinhas=0;
     char caractereLido, CharInicioPalavra=' ';
     while((caractereLido=getchar()) != EOF){
-        if(caractereLido == '\r'){
-            caractereLido = '\n';
-        }
-
+        MeSintoVelho:
         ContCaracteres++;
-
-
-
-        if(CharInicioPalavra == ' ' && VerificaSeeLetra(caractereLido)){
+        //marca o início de uma palavra
+        if(CharInicioPalavra == ' ' && VerificaSeEValido(caractereLido)){
             CharInicioPalavra = caractereLido;
         }
 
-        if(CharInicioPalavra != ' ' && (caractereLido == ' ' || caractereLido == '\n')){
+        //pega o final da palavra e conta
+        if(CharInicioPalavra != ' ' && !VerificaSeEValido(caractereLido)){
             ContPalavras++;
             CharInicioPalavra = ' ';
         }
 
-
         if(caractereLido == '\n'){
             ContLinhas++;
         }
+        if(caractereLido == '\r'){
+            caractereLido = getchar();            
+            ContLinhas++;
+            // caso seja \r\n conta só um caractere e finaliza a palavra
+            if(caractereLido == '\n'){   
+                ContCaracteres++;             
+                continue;
+            }
+            //caso seja \r'algum outro char' finaliza a linha e incializa uma nova palavra
+            printf("Velho\n");
+            goto MeSintoVelho;                        
+        }
+
     }
 
     printf("Linhas\tPalav.\tCarac.\n");
