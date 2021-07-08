cfcde4fc0c1edc5519213ebc8f540e994f3c135d
Lucasbr2312 <lucaspacheco747@gmail.com>
2021-07-07 22:28:51 -0300

    .

--- SimuladorPart/simulador.c
+++ SimuladorPart/simulador.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
 #include <stdlib.h>
+#include <unistd.h> 
 
-#define largura 64
+
+#define BLU "\e[0;34m"
+#define YEL "\e[0;33m"
+
 #define altura 32
+#define largura 64
 char matriz[altura][largura];
 char MatrizCopia[altura][largura];
 
@@ -13,50 +18,43 @@ void troca(int x1,int y1, int x2,int y2){
 }
 
 void MovimentaAgua(int i,int j){    
-    if(i+1 > altura || i+j > altura || j-1 < 0 ){
-        return;
-    }
 
-    if(matriz[i+1][j] == ' '){        
+    //se tiver ar e não for borda, faz a troca
+
+    if(matriz[i+1][j] == 32 && i+1 <= altura){     
         troca(i,j,i+1,j);
-    }else if(matriz[i+1][j-1] == ' '){
+    }else if(matriz[i+1][j-1] == 32 && i+1 <= altura && j-1 >= 0){
         troca(i,j,i+1,j-1);
-    }else if(matriz[i+1][j+1] == ' '){
+    }else if(matriz[i+1][j+1] == 32 && i+1 <= altura && j+1 <= largura){
         troca(i,j,i+1,j+1);
-    }else if(matriz[i][j-1] == ' '){
+    }else if(matriz[i][j-1] == 32 && j-1 >= 0){
         troca(i,j,i,j-1);
-    }else if(matriz[i][j+1] == ' '){
+    }else if(matriz[i][j+1] == 32 && j+1 <= largura){
         troca(i,j,i,j+1);
     }
 }
 
 void MovimentaAreia(int i,int j){   
-    if(i+1 > altura || i+j > altura || j-1 < 0 ){
+    if( i+1 > altura){
         return;
     }
-
-    if(matriz[i+1][j] == ' ' || matriz[i+1][j]=='~'){        
+    //se tiver ar ou água e não for borda, faz a troca
+    if(matriz[i+1][j] == 32 || matriz[i+1][j]==126 ){        
         troca(i,j,i+1,j);
-    }else if(matriz[i+1][j-1] == ' ' || matriz[i+1][j-1]=='~'){
+    }else if((matriz[i+1][j-1] == 32 || matriz[i+1][j-1]==126) && j-1 >= 0 ){
         troca(i,j,i+1,j-1);
-    }else if(matriz[i+1][j+1] == ' ' || matriz[i+1][j+1]=='~'){
+    }else if((matriz[i+1][j+1] == 32 || matriz[i+1][j+1]==126) && j+1 <= largura ){
         troca(i,j,i+1,j+1);
     }
 }
 
 
 
-int Fisica(){
+void Fisica(){
 
+    //Procura por areia e àgua para aplicar a física
     for(int i=0 ; i < altura; i++){
         for(int j=0; j < largura ; j++){
-            MatrizCopia[i][j] = matriz[i][j];
-        }
-    }
-
-    for(int i=0 ; i < altura; i++){
-        for(int j=0; j < largura ; j++){
-
             switch (matriz[i][j])
             {
             case '#': MovimentaAreia(i,j);
@@ -64,6 +62,12 @@ int Fisica(){
             case '~': MovimentaAgua(i,j);
                 break;
             }
+        }
+    }
+
+    for(int i=0 ; i < altura; i++){
+        for(int j=0; j < largura ; j++){
+            matriz[i][j] = MatrizCopia[i][j];
         }
     }
 }
@@ -74,7 +78,8 @@ int main(){
 
     for(int i=0 ; i < altura; i++){
         for(int j=0; j < largura ; j++){
-            matriz[i][j] = ' ';
+            matriz[i][j] = 32;
+            MatrizCopia[i][j] = 32;
         }
     }
 
@@ -82,6 +87,7 @@ int main(){
 
     while (contador < totalFrames) {
         int nLido = scanf(" %d: %d %d %c", &frame, &coordX, &coordY, &novaParticula);
+
         if(nLido == EOF){
             frame = totalFrames;
         }
@@ -91,16 +97,26 @@ int main(){
             printf("frame: %d\n", contador + 1);            
             for(int i=0;i<altura;i++){
                 for(int j=0;j<largura;j++){
-                    printf("%c",MatrizCopia[i][j]);
+                    if(matriz[i][j] == '#'){
+                        printf(YEL "%c" ,matriz[i][j]);                        
+                        continue;
+                    }else if(matriz[i][j] == '~'){
+                        printf(BLU "%c" ,matriz[i][j]);
+                        continue;
+                    }
+                    printf("%c" ,matriz[i][j]);
+
                 }
                 printf("\n");
             }
             Fisica();
             contador++;
+            usleep(20000);
         }
 
         if(nLido != EOF){
             matriz[coordY][coordX] = novaParticula;
+            MatrizCopia[coordY][coordX] = novaParticula;      
         }
     }
 }
---
+++ SimuladorPart/testes/README.txt
@@ -0,0 +1,3 @@
+Arquivo zip gerado em: 07/07/2021 20:22:45 
+Este arquivo contém os casos de teste cadastrados até o momento, disponibilizado pelo professor aos alunos.
+Exercício: [4 - Funções] Simulação de Partículas
